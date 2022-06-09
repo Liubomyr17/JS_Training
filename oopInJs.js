77a0096348e0574b9971ac965c947c07af8ef95a
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2022-06-10 02:11:05 +0300

    OOP in JS

---
+++ oopInJs.js
@@ -0,0 +1,77 @@
+class Animal {
+  constructor(name, color, numLegs) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = numLegs;
+  }
+  run() {
+    return "";
+  }
+  speak() {
+    return "";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, color, numLegs) {
+    super(name, color, numLegs);
+  }
+  run() {
+    console.log(this.name + " is running!");
+  }
+  speak() {
+    console.log("Woof-woof!");
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, color, numLegs) {
+    super(name, color, numLegs);
+  }
+  run() {
+    console.log(this.name + " is running!");
+  }
+  speak() {
+    console.log("Murrr-murrr!");
+  }
+}
+
+class Kangaroo extends Animal {
+  constructor(name, color, numLegs) {
+    super(name, color, numLegs);
+  }
+  run() {
+    console.log(this.name + " is running!");
+  }
+}
+
+class Mouse extends Animal {
+  constructor(name, color, numLegs) {
+    super(name, color, numLegs);
+  }
+  run() {
+    console.log(this.name + " is running!");
+  }
+  speak() {
+    console.log("Mi-mi-mi!");
+  }
+}
+
+let dogRex = new Dog("Rex", "black", 4);
+console.log(dogRex);
+dogRex.run();
+dogRex.speak();
+
+let catTom = new Cat("Tom", "grey", 4);
+console.log(catTom);
+catTom.run();
+catTom.speak();
+
+let kangarooJack = new Kangaroo("Jack", "redhead", 2);
+console.log(kangarooJack);
+kangarooJack.run();
+
+let mouseJerry = new Mouse("Jerry", "grey", 4);
+console.log(mouseJerry);
+mouseJerry.run();
+mouseJerry.speak();
